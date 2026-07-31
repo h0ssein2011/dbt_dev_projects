b3a8e95dc8a4b5881ad7e6df05ab039770a21339
h0ssein2011 <h0ssein2011@yahoo.com>
2026-07-31 11:10:20 +0300

    fix: correct timestamp conversion for pickup and dropoff times in order delivery model

--- models/Intermediate/Food_Delivery_dev/int_FD_dev__order_delivery_times.sql
+++ models/Intermediate/Food_Delivery_dev/int_FD_dev__order_delivery_times.sql
@@ -9,8 +9,8 @@ with source as (
 
 SELECT order_id
     , max(rider_id) as rider_id
-    , TIMESTAMP(max(case when event_type = 'pickup' then 'event_timestamp' end)) as pickup_time
-    , TIMESTAMP(max(case when event_type = 'dropoff' then 'event_timestamp' end)) as dropoff_time
+    , max(case when event_type = 'pickup' then event_timestamp end) as pickup_time
+    , max(case when event_type = 'dropoff' then event_timestamp end) as dropoff_time
 from source
 GROUP by 1
 
