f0c179e2e18294163b6624daa40b2ebfc1d25f31
Pesare shoja <pesareshoja@hi2.in>
2026-08-01 13:34:49 +0000

    fix fct_EA__experiment_funnels model minor issue

--- models/marts/Exprimentation_Analytics/fct_EA__experiment_funnels.sql
+++ models/marts/Exprimentation_Analytics/fct_EA__experiment_funnels.sql
@@ -23,7 +23,7 @@ select event_id,
     page_url
 from {{ ref('int_EA__user_sessions') }}
 )
-joined as (
+,joined as (
 select ee.user_id,
     ee.experiment_id,
     ee.variant_name,
@@ -31,6 +31,7 @@ select ee.user_id,
     max(case when ev.event_name = 'purchase' then 1 else 0 end) as is_purchase_completed
 from experiment_exposures ee
 join events ev on ee.user_id = ev.user_id and ev.timestamp > ee.true_exposure_timestamp
+group by 1,2,3  
 )
 select
 * from joined
