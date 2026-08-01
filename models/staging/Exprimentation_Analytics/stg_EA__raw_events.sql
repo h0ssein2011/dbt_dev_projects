446e4f2f7ee30ce12dce86f956f31756c73cb673
h0ssein2011 <h0ssein2011@yahoo.com>
2026-08-01 09:18:02 +0300

    feat: add initial staging models for Exprimentation Analytics

---
+++ models/staging/Exprimentation_Analytics/stg_EA__raw_events.sql
@@ -0,0 +1,24 @@
+{{
+    config(materialized='table')
+    }}
+with source as (
+ select event_id,
+    user_id,
+    event_name,
+    timestamp,
+    properties
+ from
+ {{ source('Exprimentation_Analytics_proj', 'raw_events') }}
+)
+, unnested as (
+select  event_id,
+    user_id,
+    event_name,
+    timestamp,
+    JSON_VALUE(properties,'device') as device,
+    JSON_VALUE(properties,'page_url') as page_url,
+from source
+)
+select
+*
+from unnested
