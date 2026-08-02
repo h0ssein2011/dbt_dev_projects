42c147261e15ef0241d123dc4f0aa5733ba2ea5d
h0ssein2011 <h0ssein2011@yahoo.com>
2026-08-02 19:55:40 +0300

    fix: refactor fact experiment exposures model to improve clarity and structure

--- models/marts/Exprimentation_Analytics/fct_EA__experiment_funnels.sql
+++ models/marts/Exprimentation_Analytics/fct_EA__experiment_funnels.sql
@@ -1,16 +1,16 @@
 {{
     config(materialized='table')
     }}
-with experiment_exposures as (
+with exposures as (
 
-select user_id,
+select experiment_user_id,
+    user_id,
     experiment_id,
     variant_name,
     allocated_at,
-    event_name,
-    true_exposure_timestamp,
-    device,
-    page_url
+    exposure_session_id,
+    true_exposure_timestamp
+
 from {{ ref('int_EA__experiment_exposures') }}
 where true_exposure_timestamp is not null
 )
@@ -20,18 +20,40 @@ select event_id,
     event_name,
     timestamp,
     device,
-    page_url
+    page_url,
+    coalesce(revenue,0) as revenue
 from {{ ref('int_EA__user_sessions') }}
 )
 ,joined as (
-select ee.user_id,
-    ee.experiment_id,
-    ee.variant_name,
-    max(case when ev.event_name = 'begin_checkout' then 1 else 0 end) as is_checkout_started,
-    max(case when ev.event_name = 'purchase' then 1 else 0 end) as is_purchase_completed
-from experiment_exposures ee
-join events ev on ee.user_id = ev.user_id and ev.timestamp > ee.true_exposure_timestamp
-group by 1,2,3  
+select ex.experiment_user_id,
+    ex.user_id,
+    ex.experiment_id,
+    ex.variant_name,
+    ex.allocated_at,
+    ex.exposure_session_id,
+    ex.true_exposure_timestamp,
+    e.session_id,
+    e.event_name,
+    revenue
+from exposures ex
+join events ev on ex.user_id = ev.user_id and ev.timestamp > ex.true_exposure_timestamp
+
 )
-select
-* from joined
+,aggregated as (
+select experiment_user_id,
+    user_id,
+    experiment_id,
+    variant_name,
+    allocated_at,
+    exposure_session_id,
+    true_exposure_timestamp,
+    max(case when event_name = 'begin_checkout' then 1 else 0 end) > 0 as is_checkout_started,
+    max(case when event_name = 'purchase' then 1 else 0 end) > 0 as is_purchase_completed,
+    count(distinct session_id) as post_exposure_sessions,
+    sum(case when event_name = 'purchase' then revenue else 0 end) as total_revenue
+from joined
+group by 1,2,3,4,5,6,7)
+
+
+select *
+from aggregated
