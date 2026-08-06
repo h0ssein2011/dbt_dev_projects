f58bc7e1ecad2ab9618a6f33440020ca9fa89b56
h0ssein2011 <h0ssein2011@yahoo.com>
2026-08-07 01:00:35 +0300

    fix: comment out configuration and source selection in int_SS_MRR__subscription_periods model

--- models/Intermediate/SS_MRR/int_SS_MRR__subscription_periods.sql
+++ models/Intermediate/SS_MRR/int_SS_MRR__subscription_periods.sql
@@ -1,10 +1,10 @@
-{{
-    config(materialized='table')
-    }}
+-- {{
+--     config(materialized='table')
+--     }}
 
-with source as (
- select *
- from {{ source('SS_MRR_proj', 'stg_SS_MRR__raw_subscriptions') }}
-)
-SELECT
-from source
+-- with source as (
+--  select *
+--  from {{ source('SS_MRR_proj', 'stg_SS_MRR__raw_subscriptions') }}
+-- )
+-- SELECT
+-- from source
